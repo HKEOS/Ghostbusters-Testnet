dc42469395e4ab811b79fabf137c80d2af40ea34
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-05-17 04:36:35 -0300

    Update installGhostbusters.sh

--- installGhostbusters.sh
+++ installGhostbusters.sh
@@ -1,71 +1,16 @@
 #!/bin/bash
 
-###########################################################################
-## Created by
-## Bohdan Kossak, CryptoLions.io
-##
-## Edited for Ghostbusters Testnet by
-## Igor Lins e Silva, EOS Rio
-## Jae Chung, HKEOS
-###########################################################################
+##########################################
+## Created by 				##
+## Bohdan Kossak, CryptoLions.io 	##
+## 					##
+## Edited for Ghostbusters Testnet by 	##
+## Igor Lins e Silva, EOS Rio 		##
+## Jae Chung, HKEOS  			##
+##########################################
 
-GLOBAL_PATH=$(pwd) #GLOBAL PATH
-
-TAG="DAWN-2018-05-16"
-TESTNET="ghostbusters"
-
-##### PARAMETERS TO BE MODIFIED ######
-
-# EOS Source code folder, if empty - it will create the folder, download sources and compile on the specified tag
-EOS_SOURCE_DIR="/path/to/eos"
-
-# Node port definitions (avoid ports below 1024 - you shouldn't run as root)
-NODE_API_PORT="<api-port>"
-NODE_P2P_PORT="<p2p-port>"
-# Network address, usually 0.0.0.0
-NODE_NET_ADDR="<net-addr>"
-# Externally accessible node address
-NODE_HOST="<server-address>"
-
-# HTTPS Settings - Leave port blank to disable
-NODE_SSL_PORT=""
-SSL_PRIV_KEY="/path/to/certificate-key"
-SSL_CERT_FILE="/path/to/certificate-chain"
-
-### Node Agent Name
-AGENT_NAME="<agent-name>"
-
-### IS A BLOCK PRODUCER ? ###
-ISBP=true
-
-### PRODUCER INFO ###
-PRODUCER_PUB_KEY="<pub-key>"
-PRODUCER_PRIV_KEY="";
-## PRODUCER NAME MUST HAVE 12 CHARS!
-PRODUSER_NAME="<producer-name>"
-
-### STANDARD BLOCK PRODUCER INFO - according to https://github.com/eosrio/bp-info-standard
-### Replace username with your keybase username
-PRODUCER_URL="https://<username>.keybase.pub/bp_info.json"
-
-### WALLET INFO ###
-WALLET_HOST="127.0.0.1"
-WALLET_PORT="7777"
-
-### PRE-DEFINED PEER LIST ###
-PEER_LIST='
-# p2p-peer-address = 192.168.10.Y:9876
-p2p-peer-address = <vpn-ip-address>:<p2p-port>                         
-'
-
-### PRE-DEFINED PEER KEYS ###
-PEER_KEYS='
-# peer-key = "EOS4tiVonwbmT6w5jZjxaWx8p1CkejsBtcwtn6YaqZRteKyYGQZAE"
-peer-key = "<EOS-public-key>"
-'
-
-### AUTO-GENERATED ###
-
+GLOBAL_PATH=$(pwd)
+source "$(dirname $0)/params.sh"
 NODE_HTTP_SRV_ADDR="$NODE_NET_ADDR:$NODE_API_PORT"
 NODE_P2P_LST_ENDP="$NODE_NET_ADDR:$NODE_P2P_PORT"
 NODE_P2P_SRV_ADDR="$NODE_HOST:$NODE_P2P_PORT"
@@ -75,6 +20,8 @@ if [[ $ISBP == true ]]; then
 else
 	TESTNET="$TESTNET-node"
 fi
+
+echo "Work directory: $TESTNET";
 
 ######################################################################################################################################################
 echo -n $'\E[0;32m'
@@ -109,13 +56,14 @@ if [[ $NODE_SSL_PORT == "" ]]; then
 fi
 
 PRODUCER_PRIV_KEY_DEF="!! INSERT HERE PRIVATE KEY TO THIS PUBLIC ADDRESS !!";
-TESTNET_DIR="$GLOBAL_PATH/$TESTNET"
+TESTNET_DIR="$GLOBAL_PATH/$TESTNET";
 
 if [[ $EOS_SOURCE_DIR == "" ]]; then
 	EOS_SOURCE_DIR="$GLOBAL_PATH/eos-source"
 else
+	EOS_GIT_BRANCH=$(git -C /home/entropia/eos branch | grep '*' | cut -f 5 -d' ' | cut -f1 -d')');
+	echo "Source code at branch $EOS_GIT_BRANCH";
 	EOS_VERSION=$("$EOS_SOURCE_DIR/build/programs/nodeos/nodeos" --version)
-	EOS_TARGET_VERSION="2565193790";
 	echo "Current nodeos version: $EOS_VERSION";
 	if [[ "$EOS_VERSION" != "$EOS_TARGET_VERSION" ]]; then
 		echo "Wrong version, $EOS_TARGET_VERSION required!";
@@ -371,20 +319,19 @@ if [[ $ISBP == true ]]; then
 	private-key = ["'$PRODUCER_PUB_KEY'","'$PRODUCER_PRIV_KEY'"]
 	producer-name = '$PRODUSER_NAME'
 	peer-private-key = ["'$PRODUCER_PUB_KEY'","'$PRODUCER_PRIV_KEY'"]
+	
 	' >> $TESTNET_DIR/config.ini
-else 
+else
 	echo '
 	#plugin = eosio::producer_plugin
 	#private-key = ["'$PRODUCER_PUB_KEY'","'$PRODUCER_PRIV_KEY'"]
 	#peer-private-key = ["'$PRODUCER_PUB_KEY'","'$PRODUCER_PRIV_KEY'"]
 	#producer-name = '$PRODUSER_NAME'
+	
 	' >> $TESTNET_DIR/config.ini
 fi
-
 echo "$PEER_LIST" >> $TESTNET_DIR/config.ini
-
 echo "$PEER_KEYS" >> $TESTNET_DIR/config.ini
-
 fi
 
 ###############################
@@ -478,5 +425,4 @@ echo ""
 echo "This info was saved to ghostbusters.txt file"
 echo ""
 read -n 1 -s -r -p "Press any key to continue"
-
 chmod 644 $0
