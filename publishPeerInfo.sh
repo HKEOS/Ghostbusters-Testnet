097b060d36dfd04a68fafaa42b225ed264d1d0b0
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-05-18 04:26:12 -0300

    Add files via upload

---
+++ publishPeerInfo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+GLOBAL_PATH=$(pwd)
+
+## Check KBFS mount point
+echo
+echo "--------------- VERIFYING KEYBASE FILE SYSTEM ---------------";
+echo
+
+KBFS_MOUNT=$(keybase status | grep mount | cut -f 2 -d:);
+
+## Restart Keybase if needed
+if [ -d "$KBFS_MOUNT" ]; then
+	echo "kbfs is not running!";
+	run_keybase
+	sleep 3
+else
+	printf "KBFS mounted at %s" "$KBFS_MOUNT";
+	echo
+fi
+
+keybaseUser=$(keybase status | grep Username: | cut -f2- -d: | sed -e 's/^\s*//' -e '/^$/d');
+
+echo "Keybase user = $keybaseUser";
+
+echo "Signing config file...";
+
+keybase sign -i "$1" --saltpack-version 2 -o "$keybaseUser".peer_info.signed
+
+echo "Done. File saved at $GLOBAL_PATH/$keybaseUser.peer_info.signed";
+
+openssl des3 -salt -in "$GLOBAL_PATH/$keybaseUser.peer_info.signed" -out "/keybase/team/eos_ghostbusters/mesh/$keybaseUser.peer_info.signed" -pass pass:ghostbusters
+
+
+
