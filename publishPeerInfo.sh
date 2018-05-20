a38a0af102c1fa1bf58654e724ff6b6c205695df
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-05-20 04:14:15 -0300

    publisher update
    stability fixes

--- publishPeerInfo.sh
+++ publishPeerInfo.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 GLOBAL_PATH=$(pwd)
 if [[ $1 == "" ]]; then
-	echo "Please provide the input peer_info file!";
-	exit 1;
+        echo
+        echo -e "ERROR:\nPlease provide the input peer_info file!";
+        echo
+        exit 1;
 fi
 ## Check KBFS mount point
 echo
 echo "--------------- VERIFYING KEYBASE FILE SYSTEM ---------------";
 echo
-KBFS_MOUNT=$(keybase status | grep mount | cut -f 2 -d:);
+
+KBFS_MOUNT=$(keybase status | grep mount | cut -f 2 -d: | sed -e 's/^\s*//' -e '/^$/d');
+
 ## Restart Keybase if needed
-if [ -d "$KBFS_MOUNT" ]; then
-	echo "kbfs is not running!";
-	run_keybase
-	sleep 3
+
+if [ ! -d "$KBFS_MOUNT" ]; then
+        echo "KBFS is not running!";
+        run_keybase
+        sleep 3
 else
-	printf "KBFS mounted at %s" "$KBFS_MOUNT";
-	echo
+        echo "KBFS mount point: $KBFS_MOUNT";
+        echo
 fi
+
 keybaseUser=$(keybase status | grep Username: | cut -f2- -d: | sed -e 's/^\s*//' -e '/^$/d');
+
 echo "Keybase user = $keybaseUser";
+
 echo "Signing config file...";
-keybase sign -i "$1" --saltpack-version 2 -o ~/kbfs/team/eos_ghostbusters/mesh/$keybaseUser.peer_info.signed
+
+keybase sign -i "$1" --saltpack-version 2 -o $KBFS_MOUNT/team/eos_ghostbusters/mesh/$keybaseUser.peer_info.signed
+
 echo "Done. File saved at $GLOBAL_PATH/$keybaseUser.peer_info.signed";
+
+echo -e "\n\n ---- BEGIN SIGNED FILE ---- \n";
+
+cat $GLOBAL_PATH/$keybaseUser.peer_info.signed;
+
+echo -e "\n ---- END SIGNED FILE ----";
