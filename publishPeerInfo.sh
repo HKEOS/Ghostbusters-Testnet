8340121e4c3d537b02a900fd2f6bcc2fce84a38b
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-05-20 04:42:07 -0300

    display fix

--- publishPeerInfo.sh
+++ publishPeerInfo.sh
@@ -32,10 +32,10 @@ echo "Signing config file...";
 
 keybase sign -i "$1" --saltpack-version 2 -o $KBFS_MOUNT/team/eos_ghostbusters/mesh/$keybaseUser.peer_info.signed
 
-echo "Done. File saved at $GLOBAL_PATH/$keybaseUser.peer_info.signed";
+echo "Done. File saved at $KBFS_MOUNT/team/eos_ghostbusters/mesh/$keybaseUser.peer_info.signed";
 
 echo -e "\n\n ---- BEGIN SIGNED FILE ---- \n";
 
-cat $GLOBAL_PATH/$keybaseUser.peer_info.signed;
+cat $KBFS_MOUNT/team/eos_ghostbusters/mesh/$keybaseUser.peer_info.signed;
 
 echo -e "\n ---- END SIGNED FILE ----";
