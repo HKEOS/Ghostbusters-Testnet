7ce1cc2a0080bf8b0948dc25a47e5985504a27f9
Jae Chung <jae.chung99@gmail.com>
2018-05-20 17:25:38 +0800

    Create 04_TOKEN_CREATE_ISSUE.sh

---
+++ bios-node/04_TOKEN_CREATE_ISSUE.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# Edited for Ghostbusters Testnet
+#
+################################################################################
+
+./cleos.sh push action eosio.token create '["eosio", "2500000000.0000 EOS", 0, 0, 0]' -p eosio.token
+./cleos.sh push action eosio.token issue '["eosio",  "1000000000.0000 EOS", "init"]' -p eosio
