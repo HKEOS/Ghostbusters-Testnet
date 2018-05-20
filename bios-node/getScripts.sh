c3599357e64f074a6a9832a5af504aa0278a7b8e
Jae Chung <jae.chung99@gmail.com>
2018-05-20 17:47:14 +0800

    Update getScripts.sh

--- bios-node/getScripts.sh
+++ bios-node/getScripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-svn export https://github.com/HKEOS/Ghostbusters-Testnet/trunk/bios-node bios-node
+svn export https://github.com/HKEOS/Ghostbusters-Testnet/trunk/bios-node bios-scripts
 
 for filename in ./bios-node/*.sh; do
   chmod u+x "$filename"
