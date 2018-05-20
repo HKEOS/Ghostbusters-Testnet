75c5a1597b811f943eca5ba7fe769b0e7295ccbe
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-05-20 06:12:59 -0300

    major refactor
    multi peer support

--- updatePeers.sh
+++ updatePeers.sh
@@ -1,44 +1,115 @@
 #!/bin/bash
 GLOBAL_PATH=$(pwd)
+
 ## Check KBFS mount point
 echo
 echo "--------------- VERIFYING KEYBASE FILE SYSTEM ---------------";
 echo
-KBFS_MOUNT=$(keybase status | grep mount | cut -f 2 -d:);
+
+KBFS_MOUNT=$(keybase status | grep mount | cut -f 2 -d: | sed -e 's/^\s*//' -e '/^$/d');
+
 ## Restart Keybase if needed
-if [ -d "$KBFS_MOUNT" ]; then
-	echo "kbfs is not running!";
-	run_keybase
-	sleep 3
+
+if [ ! -d "$KBFS_MOUNT" ]; then
+        echo "KBFS is not running!";
+        run_keybase
+        sleep 3
 else
-	printf "KBFS mounted at %s" "$KBFS_MOUNT";
-	echo
+        echo "KBFS mount point: $KBFS_MOUNT";
 fi
+
 myKeybaseUser=$(keybase status | grep Username: | cut -f2- -d: | sed -e 's/^\s*//' -e '/^$/d');
+
 echo "Keybase user = $myKeybaseUser";
-echo "### AUTOMATIC PEER CONFIGURATION ###" > temp_config.ini;
+
+echo "### AUTOMATIC PEER CONFIGURATION ###" > config.ini.temp;
+
+wgPeerCount=0;
+eosPeerCount=0;
+
+add_section()
+{
+	if [[ $section == "wg" ]] && [[ $publickey != "" ]] && [[ $endpoint != "" ]] && [[ $allowedips != "" ]]; then
+		echo -e "\n Injecting wg peer with:\n >> PublicKey: $publickey\n >> Endpoint: $endpoint\n >> AllowedIPs: $allowedips\n >> PKA: $persistentkeepalive\n";
+		sudo wg set ghostbusters peer "$publickey=" endpoint "$endpoint" allowed-ips "$allowedips" persistent-keepalive "$persistentkeepalive";
+		((wgPeerCount++))
+	fi
+	if [[ $section == "eos" ]]; then
+		((eosPeerCount++))
+	fi
+	section=""
+}
+
+add_eos_line()
+{
+	if [[ $line == "peer-key"* ]] || [[ $line == "p2p-peer-address"* ]]; then
+		echo "ADD EOS LINE";
+		echo "$line" >> config.ini.temp;
+	fi
+}
+
 for file in ~/kbfs/team/eos_ghostbusters/mesh/*.peer_info.signed; do
 	[ -e "$file" ] || continue
+	echo -e "\n";
 	echo "Reading data from $file";
-	kbuser=$(echo "$file" | cut -f1 -d'.' | cut -f8 -d'/');
+	kbuser=$(echo "$file" | sed -e 's/.*mesh\/\(.*\).peer_info.signed*/\1/');
 	if [[ $myKeybaseUser != $kbuser ]]; then
 		echo "Verifying signature from $kbuser";
 		cat "$file" | keybase verify -S "$kbuser" &>output
 		out=$(<output)
 		err=$(echo "$out" | grep "ERR");
 		if [[ "$err" == "" ]]; then
-			wgPubKey=$(echo "$out" | grep PublicKey | cut -f3 -d' ');
-			wgAllowedIPs=$(echo "$out" | grep AllowedIPs | cut -f3 -d' ');
-			wgEndpoint=$(echo "$out" | grep Endpoint | cut -f3 -d' ');
-			wgPKA=$(echo "$out" | grep PersistentKeepAlive | cut -f3 -d' ');
-			eosData=$(echo "$out" | grep -A 2 '\[EOS\]');
-			echo "$eosData" | grep 'p2p-peer-address' >> temp_config.ini;
-			echo "$eosData" | grep 'peer-key' >> temp_config.ini;
-			sudo wg set ghostbusters peer "$wgPubKey" endpoint "$wgEndpoint" allowed-ips "$wgAllowedIPs" persistent-keepalive "$wgPKA";
+			# echo -e "\n---- PEER DATA ----";
+			# echo "$out";
+			# echo -e "\n---- PEER DATA ----";
+			section="";
+			while read line; do
+			  if [[ $line != "" ]] && [[ $line != \#* ]]; then
+				if [[ $line == [* ]]; then
+					add_section;
+				fi
+
+				if [[ "${line,,}" == "[peer]" ]]; then
+					section="wg";
+					continue;
+				fi
+
+                                if [[ "${line,,}" == "[eos]" ]]; then
+                                        section="eos";
+					continue;
+                                fi
+
+				if [[ $section == "wg" ]]; then
+					shopt -s extglob
+					prop=$(echo "$line" | cut -f1 -d"=" | sed -e 's/^\s*//' -e '/^$/d');
+					prop="${prop,,}";
+					prop="${prop%%*( )}";
+					value=$(echo "$line" | cut -f2 -d"=" | sed -e 's/^\s*//' -e '/^$/d');
+					declare "$prop=$value";
+					shopt -u extglob
+				fi
+                                if [[ $section == "eos" ]]; then
+                                	echo "[EOS] >> $line";
+					add_eos_line;
+                                fi
+			  fi
+			done <output
 		else
-			echo "Unable to verify! Skipping...";
+			echo -e "Unable to verify! Skipping...";
 		fi
+	else
+		echo -e "Hey, that's you!\n";
 	fi
 done
-# Save keybase config
+
+# Save wg config
 sudo wg-quick save ghostbusters;
+
+# Display wg
+echo -e "\n-------- WIREGUARD INTERFACE ---------";
+sudo wg show ghostbusters;
+
+echo -e "\n-------- EOS CONFIG DATA ---------";
+
+cat config.ini.temp;
+echo -e "\nWG Peers: $wgPeerCount \nEOS Peers: $eosPeerCount";
