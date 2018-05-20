b54d49b395b72ca2a8fe9db9129b7c0bb94c4493
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-05-20 03:22:44 -0300

    added publish and update scripts

--- setup.sh
+++ setup.sh
@@ -4,6 +4,14 @@ echo "Downloading latest install script...";
 curl -s -O https://raw.githubusercontent.com/HKEOS/Ghostbusters-Testnet/master/installGhostbusters.sh
 chmod u+x installGhostbusters.sh
 
+echo "Downloading latest publishPeerInfo script...";
+curl -s -O https://raw.githubusercontent.com/HKEOS/Ghostbusters-Testnet/master/publishPeerInfo.sh
+chmod u+x publishPeerInfo.sh
+
+echo "Downloading latest updatePeers script...";
+curl -s -O https://raw.githubusercontent.com/HKEOS/Ghostbusters-Testnet/master/updatePeers.sh
+chmod u+x updatePeers.sh
+
 if [[ ! -f ./params.sh ]]; then
   echo "Downloading sample params...";
   curl -s -O https://raw.githubusercontent.com/HKEOS/Ghostbusters-Testnet/master/params.sh
