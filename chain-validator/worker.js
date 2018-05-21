1da53bf9c007ff91018f5f6a148ea4119b6e5ab5
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-05-21 10:33:37 -0300

    cpu core sync, IPC reduction

--- chain-validator/worker.js
+++ chain-validator/worker.js
@@ -2,7 +2,6 @@ const Eos = require('eosjs');
 const mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 mongoose.Promise = require('bluebird');
-
 const config = {
     keyProvider: [],
     httpEndpoint: 'http://aurora.eosrio.io:28888',
@@ -12,7 +11,7 @@ const config = {
     sign: false
 };
 const eos = Eos.Localnet(config);
-
+let tempBlocks = 0;
 mongoose.connect('mongodb://localhost/eos_mainnet').then(() => {
     process.send({
         status: 'ready'
@@ -20,28 +19,30 @@ mongoose.connect('mongodb://localhost/eos_mainnet').then(() => {
 }, (err) => {
     console.log(err);
 });
-
 const BlockSchema = new Schema({
     blk_id: {type: String, unique: true},
     prev_id: String,
     blk_num: Number
 });
 const Block = mongoose.model('block', BlockSchema);
-
 function fetchBlockRecursively(blk, limit, idx) {
     eos['getBlock']({
         block_num_or_id: blk
     }).then((result) => {
-        // console.log(result['timestamp'] + " | " + result['block_num']);
         new Block({
             blk_id: result['id'],
             prev_id: result['previous'],
             blk_num: result['block_num']
         }).save().then(() => {
             if (result['block_num'] > limit) {
-                process.send({
-                    status: 'block'
-                });
+                tempBlocks++;
+                if(tempBlocks > 100) {
+                    process.send({
+                        status: 'block',
+                        count:tempBlocks
+                    });
+                    tempBlocks = 0;
+                }
                 fetchBlockRecursively(result['previous'], limit, idx)
             } else {
                 console.log('Process reached limit');
@@ -58,7 +59,6 @@ function fetchBlockRecursively(blk, limit, idx) {
         });
     });
 }
-
 function findLastInserted(current, limit) {
     console.log('Searching for lower number inserted...');
     const query = Block.find({blk_num: {"$lt": current, "$gt": limit}}).sort({blk_num: 1}).limit(1);
@@ -75,12 +75,10 @@ function findLastInserted(current, limit) {
         console.error(err);
     });
 }
-
 process.on('message', (m) => {
     console.log(" Worker #" + (m.index + 1) + " started! - From " + m.high + " to " + m.low);
     fetchBlockRecursively(m.high, m.low, m.index);
 });
-
 process.on('beforeExit', (code) => {
     console.log(`About to exit with code: ${code}`);
 });
