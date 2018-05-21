a2f9ed10b75f378628474b320c80f5846ffafb1c
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-05-21 06:50:53 -0300

    recursive block fetching

--- chain-validator/index.js
+++ chain-validator/index.js
@@ -1,9 +1,43 @@
 const mongoose = require('mongoose');
 const Eos = require('eosjs');
-
-mongoose.connect('mongodb://localhost/test');
+let eos = null;
+mongoose.connect('mongodb://localhost/eos_mainnet');
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log('DB connection ready!');
+db.on('error', (err) => {
+    console.log(err);
+    process.exit(1);
 });
+db.once('open', () => {
+    console.log('DB connection ready!');
+    initEOSJS();
+});
+
+function initEOSJS() {
+    config = {
+        keyProvider: [],
+        httpEndpoint: 'http://aurora.eosrio.io:28888',
+        expireInSeconds: 60,
+        broadcast: true,
+        debug: false,
+        sign: false
+    };
+    eos = Eos.Localnet(config);
+    eos.getInfo({}).then(result => {
+        console.log(result);
+        // Get last irreversible block
+        const lib_num = result['last_irreversible_block_num'];
+        fetchBlockRecursively(lib_num);
+    });
+}
+
+function fetchBlockRecursively(blk) {
+    eos.getBlock({
+        block_num_or_id: blk
+    }).then((result) => {
+        // console.log(result);
+        console.log(result['timestamp'] + " | " + result['block_num']);
+        if (result['block_num'] > 1) {
+            fetchBlockRecursively(result['previous'])
+        }
+    });
+}
