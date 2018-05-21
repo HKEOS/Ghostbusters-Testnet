dfe9b7885ddf08cbff4dfab35432bfb4743b2a4f
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-05-21 04:21:09 -0300

    interactive publish pre-release

--- interactivePublish.sh
+++ interactivePublish.sh
@@ -3,73 +3,202 @@ np=1;
 echo -e "\n\n----- >> Interactive peer info builder << ------\n";
 
 if [[ $1 != "new" ]]; then
-
 	if [[ -f ./peerInfo.temp ]]; then
-		source peerInfo.temp
-		echo "Loaded config...";
-		echo "WG_IP: $WG_IP";
-		echo "WG_PublicKey: $WG_PublicKey";
-		echo "WG_Endpoint: $WG_Endpoint";
+		source peerInfo.temp;
+		cat peerInfo.temp | grep "_$np_";
 	else
 		echo -e "## Temp settings" > peerInfo.temp;
 	fi
-
+else
+	echo -e "## Temp settings" > peerInfo.temp;
 fi
 
-new_wg_peer
+inc() {
+	((np++))
+}
 
-if [[ $WG_IP == "" ]]; then
-	echo -e " > WG Peer $np ip address [192.168.100.0 to 192.168.103.255]: \c"
-	read
-	if [[ "$REPLY" = "" ]]; then
-		exit 1;
-	else
-		WG_IP="$REPLY";
-	 # ls ~/kbfs/team/eos_ghostbusters/ip_list/ | grep $WG_IP
-	 echo "WG_IP=\"$WG_IP\"" >> peerInfo.temp;
-	fi  
-fi
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
 
-if [[ $WG_PublicKey == "" ]]; then
-	echo -e " > WG Peer $np PublicKey: \c"
-	read
-	if [[ "$REPLY" = "" ]]; then
-		exit 1;
-	else
-		WG_PublicKey="$REPLY";
-	 echo "WG_PublicKey=\"$WG_PublicKey\"" >> peerInfo.temp;
-	fi  
-fi
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
 
-if [[ $WG_PublicKey == "" ]]; then
-	echo -e " > WG Peer $np PublicKey: \c"
-	read
-	if [[ "$REPLY" = "" ]]; then
-		exit 1;
-	else
-		WG_PublicKey="$REPLY";
-	 echo "WG_PublicKey=\"$WG_PublicKey\"" >> peerInfo.temp;
-	fi  
-fi
+set_peer_number() {
+	echo "Editing peer number: $1";
+	np="$1";
+}
 
-if [[ $WG_Endpoint == "" ]]; then
-	echo -e " > WG Peer $np Endpoint: \c"
+edit() {
+	max=8
+	echo -e " > Select a peer number to edit: 1-$max \c"
 	read
-	if [[ "$REPLY" = "" ]]; then
-		exit 1;
-	else
-		WG_Endpoint="$REPLY";
-	 echo "WG_Endpoint=\"$WG_Endpoint\"" >> peerInfo.temp;
-	fi  
-fi
+	set_peer_number "$REPLY";
+}
 
-if [[ $WG_PKA == "" ]]; then
-	echo -e " > WG Peer $np PersistentKeepAlive [default=20]: \c"
+choose_action() {
+	echo -e "\n --- MENU ---\n";
+	PS3=' > Please select your action: '
+	options=("New Peer" "Edit" "Done")
+	select opt in "${options[@]}"
+	do
+		case $opt in
+			"New Peer")
+				echo "New peer selected";
+				new=true;
+				((np++))
+				new_peer;
+				;;
+			"Edit")
+				echo "Edit peers"
+				edit;
+				;;
+			"Done")
+				save;
+				echo -e "\n > Quitting...";
+				exit 1;
+				break
+				;;
+			*) echo invalid option;;
+		esac
+	done
+}
+
+convert() {
+	current_peer=0;
+	current_cat="";
+	while read line; do
+		if [[ $line != "" ]] && [[ $line != \#* ]]; then
+			key=$(echo "$line" | cut -f1 -d'=');
+			value=$(echo "$line" | cut -f2 -d'=');
+			cat=$(echo "$key" | cut -f1 -d'_');
+			pos=$(echo "$key" | cut -f2 -d'_');
+			field=$(echo "$key" | cut -f3 -d'_');
+			if [[ $cat != $current_cat ]] || (( pos != current_peer)); then
+				current_peer="$pos";
+				current_cat="$cat";
+				if [[ $cat == "EOS" ]]; then
+					echo "[EOS]";
+				fi
+				if [[ $cat == "WG" ]]; then
+					echo "[Peer]";
+				fi
+			fi
+			echo "[$cat : $pos] - $field = $value";
+		fi
+	done < peerInfo.temp
+}
+
+save() {
+	echo -e " > Do you want to save your settings? [y/n] \c"
 	read
-	if [[ "$REPLY" = "" ]]; then
-		WG_PKA="20";
+	if [[ "$REPLY" = "y" || "$REPLY" = "" ]]; then
+		echo -e "\nWriting to file...";
+
+		echo -e "\n------- START -------\n";
+		convert;
+		echo -e "\n------- END ---------"
 	else
-		WG_PKA="$REPLY";
+		echo "my-peer-info wasn't touched!";
 	fi
-	echo "WG_PKA=\"$WG_PKA\"" >> peerInfo.temp;
-fi
+}
+
+edit_wg() {
+	echo -e " > WG Peer $np ip address [192.168.100.0 to 192.168.103.255]\n Current value: ${var[WG_IP_$np]} \c"
+	read
+	let "WG_IP_$np"="$REPLY";
+}
+
+wg_ip() {
+	varname='WG_'$np'_IP'
+	if [[ "${!varname}" == "" || $new == true ]]; then
+		echo -e " > WG Peer $np ip address [192.168.100.0 to 192.168.103.255]: \c"
+		read
+		if valid_ip $REPLY; then
+			if [[ "$REPLY" = "" ]]; then
+				exit 1;
+			else
+				declare "$varname"="$REPLY";
+				echo "$varname";
+				echo "${!varname}";
+				# ls ~/kbfs/team/eos_ghostbusters/ip_list/ | grep $WG_IP
+				echo "$varname=\"${!varname}\"" >> peerInfo.temp;
+			fi 
+		else
+			echo -e "\n Invalid IP Address: $REPLY \n"
+			wg_ip;
+		fi
+	fi
+}
+
+wg_pubkey() {
+	varname='WG_'$np'_PublicKey'
+	if [[ "${!varname}" == "" || $new == true ]]; then
+		echo -e " > WG Peer $np PublicKey: \c"
+		read
+		if [[ "$REPLY" = "" ]]; then
+			exit 1;
+		else
+			declare "$varname"="$REPLY";
+			echo "$varname";
+			echo "${!varname}";
+			echo "$varname=\"${!varname}\"" >> peerInfo.temp;
+		fi 
+	fi
+}
+
+wg_endpoint() {
+	varname='WG_'$np'_Endpoint'
+	if [[ "${!varname}" == "" || $new == true ]]; then
+		echo -e " > WG Peer $np Endpoint [host:port]: \c"
+		read
+		if [[ "$REPLY" = "" ]]; then
+			exit 1;
+		else
+			declare "$varname"="$REPLY";
+			echo "$varname";
+			echo "${!varname}";
+			echo "$varname=\"${!varname}\"" >> peerInfo.temp;
+		fi 
+	fi
+}
+
+wg_pka() {
+	varname='WG_'$np'_PKA'
+	if [[ "${!varname}" == "" || $new == true ]]; then
+		echo -e " > WG Peer $np PersistentKeepAlive [default=20]: \c"
+		read
+		if [[ "$REPLY" = "" ]]; then
+			declare "$varname"="20";
+		else
+			declare "$varname"="$REPLY";
+			echo "$varname";
+			echo "${!varname}";
+		fi 
+		echo "$varname=\"${!varname}\"" >> peerInfo.temp;
+	fi
+}
+
+new_peer() {
+	np=$(cat peerInfo.temp | grep -o "_[0-9]" | sort | uniq | wc -l);
+	inc;
+	# Wireguard data
+	wg_ip;
+	wg_pubkey;
+	wg_endpoint;
+	wg_pka;
+	# EOS data
+	choose_action;
+}
+
+choose_action;
