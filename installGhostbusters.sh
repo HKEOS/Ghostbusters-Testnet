b737056691eff0ac1ec0aed6cf88106e584cf366
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-05-22 11:05:55 -0300

    unlinked producer name

--- installGhostbusters.sh
+++ installGhostbusters.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ##########################################
-## Created by 				            ##
-## Bohdan Kossak, CryptoLions.io 	    ##
-## 					                    ##
-## Edited for Ghostbusters Testnet by 	##
-## Igor Lins e Silva, EOS Rio 		    ##
-## Jae Chung, HKEOS  			        ##
+## Created by                           ##
+## Bohdan Kossak, CryptoLions.io        ##
+##                                      ##
+## Edited for Ghostbusters Testnet by   ##
+## Igor Lins e Silva, EOS Rio           ##
+## Jae Chung, HKEOS                     ##
 ##########################################
 
 GLOBAL_PATH=$(pwd)
@@ -17,9 +17,9 @@ NODE_P2P_SRV_ADDR="$NODE_HOST:$NODE_P2P_PORT"
 NODE_HTTPS_SERVER_ADDR="$NODE_HOST:$NODE_SSL_PORT"
 
 if [[ $ISBP == true ]]; then
-	TESTNET="$TESTNET-$PRODUCER_NAME"
+    TESTNET="$TESTNET-$PRODUCER_NAME"
 else
-	TESTNET="$TESTNET-node"
+    TESTNET="$TESTNET-node"
 fi
 
 echo "$TESTNET" > testnet.name;
@@ -31,45 +31,45 @@ echo "Work directory: $TESTNET";
 # Validations
 
 if [[ $ISBP == true ]]; then
-	if [[ $PRODUCER_NAME == "<producer-name>" || $PRODUCER_NAME == "" ]]; then
-		echo "Please define a producer name!";
-		exit 1;
-	fi
-	if [[ ${#PRODUCER_NAME} != 12 ]]; then
-		echo "Producer name must be exactly 12 characters long!";
-		exit 1;
-	fi	
-	if [[ $AGENT_NAME == "<producer-name>" || $AGENT_NAME == "" ]]; then
-		echo "Please define an agent name!";
-		exit 1;
-	fi
-	if [[ $PRODUCER_PUB_KEY == "<pub-key>" || $PRODUCER_PUB_KEY == "" ]]; then
-		echo "Please define a producer public key!";
-		exit 1;
-	fi
+    if [[ $PRODUCER_NAME == "<producer-name>" || $PRODUCER_NAME == "" ]]; then
+        echo "Please define a producer name!";
+        exit 1;
+    fi
+    if [[ ${#PRODUCER_NAME} != 12 ]]; then
+        echo "Producer name must be exactly 12 characters long!";
+        exit 1;
+    fi
+    if [[ $AGENT_NAME == "<producer-name>" || $AGENT_NAME == "" ]]; then
+        echo "Please define an agent name!";
+        exit 1;
+    fi
+    if [[ $PRODUCER_PUB_KEY == "<pub-key>" || $PRODUCER_PUB_KEY == "" ]]; then
+        echo "Please define a producer public key!";
+        exit 1;
+    fi
 fi
 
 if [[ $NODE_SSL_PORT == "" ]]; then
-	if [[ $NODE_API_PORT == "<api-port>" || $NODE_API_PORT == "" ]]; then
-		echo "Please define a http api port!";
-		exit 1;
-	fi
+    if [[ $NODE_API_PORT == "<api-port>" || $NODE_API_PORT == "" ]]; then
+        echo "Please define a http api port!";
+        exit 1;
+    fi
 fi
 
 PRODUCER_PRIV_KEY_DEF="!! INSERT HERE PRIVATE KEY TO THIS PUBLIC ADDRESS !!";
 TESTNET_DIR="$GLOBAL_PATH/$TESTNET";
 
 if [[ $EOS_SOURCE_DIR == "" ]]; then
-	EOS_SOURCE_DIR="$GLOBAL_PATH/eos-source"
+    EOS_SOURCE_DIR="$GLOBAL_PATH/eos-source"
 else
-	EOS_GIT_BRANCH=$(git -C $EOS_SOURCE_DIR branch | grep '*' | cut -f 5 -d' ' | cut -f1 -d')');
-	echo "Source code at branch $EOS_GIT_BRANCH";
-	EOS_VERSION=$("$EOS_SOURCE_DIR/build/programs/nodeos/nodeos" --version)
-	echo "Current nodeos version: $EOS_VERSION";
-	if [[ "$EOS_VERSION" != "$EOS_TARGET_VERSION" ]]; then
-		echo "Wrong version, $EOS_TARGET_VERSION required!";
-		exit 1
-	fi
+    EOS_GIT_BRANCH=$(git -C $EOS_SOURCE_DIR branch | grep '*' | cut -f 5 -d' ' | cut -f1 -d')');
+    echo "Source code at branch $EOS_GIT_BRANCH";
+    EOS_VERSION=$("$EOS_SOURCE_DIR/build/programs/nodeos/nodeos" --version)
+    echo "Current nodeos version: $EOS_VERSION";
+    if [[ "$EOS_VERSION" != "$EOS_TARGET_VERSION" ]]; then
+        echo "Wrong version, $EOS_TARGET_VERSION required!";
+        exit 1
+    fi
 fi
 
 WALLET_DIR="$GLOBAL_PATH/wallet"
@@ -77,24 +77,24 @@ WALLET_DIR="$GLOBAL_PATH/wallet"
 # Download sources
 
 if [[ ! -d $EOS_SOURCE_DIR ]]; then
-	echo "..:: Downloading EOS Sources ::..";
-	mkdir $EOS_SOURCE_DIR
-	cd $EOS_SOURCE_DIR
+    echo "..:: Downloading EOS Sources ::..";
+    mkdir $EOS_SOURCE_DIR
+    cd $EOS_SOURCE_DIR
 
-	git clone https://github.com/eosio/eos --recursive .
-	git checkout $TAG
-	git submodule update --init --recursive
-	cd $GLOBAL_PATH
+    git clone https://github.com/eosio/eos --recursive .
+    git checkout $TAG
+    git submodule update --init --recursive
+    cd $GLOBAL_PATH
 fi
 
 
 # Compile Sources
 if [[ ! -d $EOS_SOURCE_DIR/build ]]; then
-	echo "..:: Compiling EOS Sources ::..";
-	cd $EOS_SOURCE_DIR
-	git pull
-	./eosio_build.sh
-	cd $GLOBAL_PATH
+    echo "..:: Compiling EOS Sources ::..";
+    cd $EOS_SOURCE_DIR
+    git pull
+    ./eosio_build.sh
+    cd $GLOBAL_PATH
 fi
 
 # Creating Wallet Folder and files
@@ -112,10 +112,10 @@ signature='#!/bin/bash
 #######################################################\n\n';
 
 if [[ ! -d $WALLET_DIR ]]; then
-	echo "..:: Creating Wallet Dir: $WALLET_DIR ::..";
-	mkdir $WALLET_DIR
+    echo "..:: Creating Wallet Dir: $WALLET_DIR ::..";
+    mkdir $WALLET_DIR
 
-	echo "..:: Creating Wallet start.sh ::..";
+    echo "..:: Creating Wallet start.sh ::..";
     # Creating start.sh for wallet
     echo -ne "$signature" > $WALLET_DIR/start.sh
     echo "DATADIR=$WALLET_DIR" >> $WALLET_DIR/start.sh
@@ -130,19 +130,19 @@ if [[ ! -d $WALLET_DIR ]]; then
     echo "DIR=$WALLET_DIR" >> $WALLET_DIR/stop.sh
     echo '
     if [ -f $DIR"/wallet.pid" ]; then
-    	pid=$(cat $DIR"/wallet.pid")
-    	echo $pid
-    	kill $pid
-    	rm -r $DIR"/wallet.pid"
+        pid=$(cat $DIR"/wallet.pid")
+        echo $pid
+        kill $pid
+        rm -r $DIR"/wallet.pid"
 
-    	echo -ne "Stopping Wallet"
+        echo -ne "Stopping Wallet"
 
-    	while true; do
-    		[ ! -d "/proc/$pid/fd" ] && break
-    		echo -ne "."
-    		sleep 1
-    	done
-    	echo -ne "\rWallet stopped. \n"
+        while true; do
+            [ ! -d "/proc/$pid/fd" ] && break
+            echo -ne "."
+            sleep 1
+        done
+        echo -ne "\rWallet stopped. \n"
 
     fi
     ' >>  $WALLET_DIR/stop.sh
@@ -153,16 +153,16 @@ fi
 #start Wallet
 echo "..:: Starting Wallet ::.."
 if [[ ! -f $WALLET_DIR/wallet.pid ]]; then
-	$WALLET_DIR/start.sh
+    $WALLET_DIR/start.sh
 fi
 
 #################### TESTNET #################################
 
 # Creating TestNet Folder and files
 if [[ ! -d $TESTNET_DIR ]]; then
-	echo "..:: Creating Testnet Dir: $TESTNET_DIR ::..";
+    echo "..:: Creating Testnet Dir: $TESTNET_DIR ::..";
 
-	mkdir $TESTNET_DIR
+    mkdir $TESTNET_DIR
 
     # Creating node start.sh 
     echo "..:: Creating start.sh ::..";
@@ -183,19 +183,19 @@ if [[ ! -d $TESTNET_DIR ]]; then
     echo -ne "\n";
     echo '
     if [ -f $DIR"/nodeos.pid" ]; then
-    	pid=$(cat $DIR"/nodeos.pid")
-    	echo $pid
-    	kill $pid
-    	rm -r $DIR"/nodeos.pid"
+        pid=$(cat $DIR"/nodeos.pid")
+        echo $pid
+        kill $pid
+        rm -r $DIR"/nodeos.pid"
 
-    	echo -ne "Stopping Nodeos"
+        echo -ne "Stopping Nodeos"
 
-    	while true; do
-    		[ ! -d "/proc/$pid/fd" ] && break
-    		echo -ne "."
-    		sleep 1
-    	done
-    	echo -ne "\rNodeos stopped. \n"
+        while true; do
+            [ ! -d "/proc/$pid/fd" ] && break
+            echo -ne "."
+            sleep 1
+        done
+        echo -ne "\rNodeos stopped. \n"
 
     fi
     ' >>  $TESTNET_DIR/stop.sh
@@ -208,9 +208,9 @@ if [[ ! -d $TESTNET_DIR ]]; then
     echo "CLEOS=$EOS_SOURCE_DIR/build/programs/cleos/cleos" >> $TESTNET_DIR/cleos.sh
     echo -ne "\n"
     if [[ $NODE_SSL_PORT != "" ]]; then
-    	echo "\$CLEOS -u https://127.0.0.1:$NODE_SSL_PORT --wallet-url http://127.0.0.1:$WALLET_PORT \"\$@\"" >> $TESTNET_DIR/cleos.sh
+        echo "\$CLEOS -u https://127.0.0.1:$NODE_SSL_PORT --wallet-url http://127.0.0.1:$WALLET_PORT \"\$@\"" >> $TESTNET_DIR/cleos.sh
     else
-    	echo "\$CLEOS -u http://127.0.0.1:$NODE_API_PORT --wallet-url http://127.0.0.1:$WALLET_PORT \"\$@\"" >> $TESTNET_DIR/cleos.sh
+        echo "\$CLEOS -u http://127.0.0.1:$NODE_API_PORT --wallet-url http://127.0.0.1:$WALLET_PORT \"\$@\"" >> $TESTNET_DIR/cleos.sh
 
     fi
     chmod u+x $TESTNET_DIR/cleos.sh
@@ -238,31 +238,31 @@ if [[ ! -d $TESTNET_DIR ]]; then
 # config.ini 
 echo -ne "\n\n..:: Creating config.ini ::..\n\n";
 if [[ $ISBP == true && $PRODUCER_PRIV_KEY == "" ]]; then 
-	echo -n $'\E[0;33m'
-	echo "!!! PRIV KEY SECTION !!! You can enter your private key here and it will be imported in wallet and inserted in config.ini. I can skip this step (Enter) and do it manually before start"
-	echo -ne "PRIV KEY (Enter skip):"
-	read PRODUCER_PRIV_KEY
-	echo -n $'\E[0;37m'
+    echo -n $'\E[0;33m'
+    echo "!!! PRIV KEY SECTION !!! You can enter your private key here and it will be imported in wallet and inserted in config.ini. I can skip this step (Enter) and do it manually before start"
+    echo -ne "PRIV KEY (Enter skip):"
+    read PRODUCER_PRIV_KEY
+    echo -n $'\E[0;37m'
 fi
 
 if [[ $ISBP == true ]]; then
-	if [[ $PRODUCER_PRIV_KEY == "" ]]; then 
-		PRODUCER_PRIV_KEY=$PRODUCER_PRIV_KEY_DEF
-	else 
-		if [[ ! -f $WALLET_DIR/default.wallet ]]; then
-			WALLET_LOG=$( $TESTNET_DIR/cleos.sh wallet create)
-			echo "$WALLET_LOG" > wallet_pass.txt
-		fi
-	fi
-	$TESTNET_DIR/cleos.sh wallet import $PRODUCER_PRIV_KEY	
+    if [[ $PRODUCER_PRIV_KEY == "" ]]; then 
+        PRODUCER_PRIV_KEY=$PRODUCER_PRIV_KEY_DEF
+    else 
+        if [[ ! -f $WALLET_DIR/default.wallet ]]; then
+            WALLET_LOG=$( $TESTNET_DIR/cleos.sh wallet create)
+            echo "$WALLET_LOG" > wallet_pass.txt
+        fi
+    fi
+    $TESTNET_DIR/cleos.sh wallet import $PRODUCER_PRIV_KEY  
 fi
 
 if [[ $PEER_PUB_KEY == "" ]]; then
-	PEER_PUB_KEY="$PRODUCER_PUB_KEY";
+    PEER_PUB_KEY="$PRODUCER_PUB_KEY";
 fi
 
 if [[ $PEER_PRIV_KEY == "" ]]; then
-	PEER_PRIV_KEY="$PRODUCER_PRIV_KEY";
+    PEER_PRIV_KEY="$PRODUCER_PRIV_KEY";
 fi
 
 echo "### EOS Ghostbusters Testnet Config file. Autogenerated by script." > $BASE_CONFIG
@@ -278,7 +278,7 @@ access-control-allow-origin = *
 ' >> $BASE_CONFIG
 
 if [[ $NODE_SSL_PORT != "" ]]; then
-	echo '
+    echo '
 # SSL
 # Filename with https private key in PEM format. Required for https (eosio::http_plugin)
 https-server-address = '$NODE_HTTPS_SERVER_ADDR'
@@ -286,9 +286,9 @@ https-server-address = '$NODE_HTTPS_SERVER_ADDR'
 https-certificate-chain-file = '$SSL_CERT_FILE'
 # Filename with https private key in PEM format. Required for https (eosio::http_plugin)
 https-private-key-file = '$SSL_PRIV_KEY'
-	' >> $BASE_CONFIG
+    ' >> $BASE_CONFIG
 else
-	echo '
+    echo '
 # SSL
 # Filename with https private key in PEM format. Required for https (eosio::http_plugin)
 # https-server-address =
@@ -296,7 +296,7 @@ else
 # https-certificate-chain-file =
 # Filename with https private key in PEM format. Required for https (eosio::http_plugin)
 # https-private-key-file =
-	' >> $BASE_CONFIG
+    ' >> $BASE_CONFIG
 fi
 
 
@@ -325,13 +325,13 @@ agent-name = '$AGENT_NAME'
 echo 'peer-private-key = ["'$PEER_PUB_KEY'","'$PEER_PRIV_KEY'"]' >> $BASE_CONFIG;
 
 if [[ $ISBP == true ]]; then
-	echo 'private-key = ["'$PRODUCER_PUB_KEY'","'$PRODUCER_PRIV_KEY'"]' >> $BASE_CONFIG;
-	echo 'producer-name = $PRODUCER_NAME' >> $BASE_CONFIG;
-	echo 'plugin = eosio::producer_plugin' >> $BASE_CONFIG;
+    echo 'private-key = ["'$PRODUCER_PUB_KEY'","'$PRODUCER_PRIV_KEY'"]' >> $BASE_CONFIG;
+    echo "producer-name = $PRODUCER_NAME" >> $BASE_CONFIG;
+    echo "plugin = eosio::producer_plugin" >> $BASE_CONFIG;
 else
-	echo '# private-key = ["'$PRODUCER_PUB_KEY'","'$PRODUCER_PRIV_KEY'"]' >> $BASE_CONFIG;
-	echo '# producer-name = $PRODUCER_NAME' >> $BASE_CONFIG;
-	echo '# plugin = eosio::producer_plugin' >> $BASE_CONFIG;
+    echo '# private-key = ["'$PRODUCER_PUB_KEY'","'$PRODUCER_PRIV_KEY'"]' >> $BASE_CONFIG;
+    echo "# producer-name = $PRODUCER_NAME" >> $BASE_CONFIG;
+    echo "# plugin = eosio::producer_plugin" >> $BASE_CONFIG;
 fi
 echo "$PEER_LIST" >> $BASE_CONFIG
 echo "$PEER_KEYS" >> $BASE_CONFIG
@@ -410,9 +410,9 @@ FINISHTEXT+=" | bp02_stakeTokens.sh..........stake tokens\n"
 FINISHTEXT+=" | .............................Use it to stake tokens before voting\n"
 FINISHTEXT+=" |\n"
 FINISHTEXT+=" | bp03_voteProducer.sh.........vote example\n"
-FINISHTEXT+=" |	.............................It will vote only in yourself.\n"
-FINISHTEXT+=" |	.............................You can add other producers\n"
-FINISHTEXT+=" |	.............................manually editing the script\n"
+FINISHTEXT+=" | .............................It will vote only in yourself.\n"
+FINISHTEXT+=" | .............................You can add other producers\n"
+FINISHTEXT+=" | .............................manually editing the script\n"
 FINISHTEXT+=" |\n"
 FINISHTEXT+=" | bp04_claimReward.sh..........claim producer rewards.\n"
 FINISHTEXT+=" |\n"
