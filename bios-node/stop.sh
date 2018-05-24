003c1d97c7f6bdd01fdd130c65cf0710cfd05f14
Jae Chung <jae.chung99@gmail.com>
2018-05-24 17:53:24 +0800

    Create stop.sh

---
+++ bios-node/stop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#######################################################
+##                                                   ##
+## Script Created by CryptoLions, HKEOS and EOS Rio  ##
+## For EOS Ghostbusters Testnet                      ##
+##                                                   ##
+## https://github.com/CryptoLions                    ##
+## https://github.com/eosrio                         ##
+## https://github.com/HKEOS/Ghostbusters-Testnet     ##
+##                                                   ##
+#######################################################
+
+DIR=$(pwd)
+
+    if [ -f $DIR"/nodeos.pid" ]; then
+    	pid=$(cat $DIR"/nodeos.pid")
+    	echo $pid
+    	kill $pid
+    	rm -r $DIR"/nodeos.pid"
+
+    	echo -ne "Stopping Nodeos"
+
+    	while true; do
+    		[ ! -d "/proc/$pid/fd" ] && break
+    		echo -ne "."
+    		sleep 1
+    	done
+    	echo -ne "\rNodeos stopped. \n"
+
+    fi
