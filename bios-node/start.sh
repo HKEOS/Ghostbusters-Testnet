e1169e3f39df1e58aad6e1d9dba16f83455bf7b3
Jae Chung <jae.chung99@gmail.com>
2018-05-24 17:52:00 +0800

    Create start.sh

---
+++ bios-node/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#######################################################
+##                                                   ##
+## Script Created by CryptoLions, HKEOS and EOS Rio  ##
+## For EOS Ghostbusters Testnet                      ##
+##                                                   ##
+## https://github.com/CryptoLions                    ##
+## https://github.com/eosrio                         ##
+## https://github.com/HKEOS/Ghostbusters-Testnet     ##
+##                                                   ##
+#######################################################
+
+NODEOS=/home/ubuntu/eos-may/eos/build/programs/nodeos/nodeos
+DATADIR=$(pwd)
+$DATADIR/stop.sh
+$NODEOS --data-dir $DATADIR --config-dir $DATADIR "$@" > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
