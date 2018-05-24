7be546bc17510adcb7904c7d7069b5037798a394
Jae Chung <jae.chung99@gmail.com>
2018-05-24 18:40:25 +0800

    Update 05_SYSTEM_CONTRACTS.sh

--- bios-node/05_SYSTEM_CONTRACTS.sh
+++ bios-node/05_SYSTEM_CONTRACTS.sh
@@ -9,5 +9,5 @@
 SOURCES_FOLDER="$( jq -r '.SOURCES_FOLDER' "00_CONFIG.conf" )"
 CONTRACTS_FOLDER="$SOURCES_FOLDER/build/contracts"
 
-./cleos.sh set contract eosio $CONTRACTS_FOLDER/eosio.system -p eosio
+./cleos.sh set contract eosio $CONTRACTS_FOLDER/eosio.system -x 1000 -p eosio
 ./cleos.sh push action eosio setpriv '["eosio.msig",1]' -p eosio
