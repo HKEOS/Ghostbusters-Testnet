b013a3e09c8e77fc865efba49017c14a4d262ebb
Jae Chung <jae.chung99@gmail.com>
2018-05-24 17:58:43 +0800

    Update start.sh

--- bios-node/start.sh
+++ bios-node/start.sh
@@ -10,7 +10,8 @@
 ##                                                   ##
 #######################################################
 
-NODEOS=/home/ubuntu/eos-may/eos/build/programs/nodeos/nodeos
+EOS_DIR="$( jq -r '.EOS_SOURCE_DIR' "00_CONFIG.conf" )"
+NODEOS=$EOS_DIR/build/programs/nodeos/nodeos
 DATADIR=$(pwd)
 $DATADIR/stop.sh
 $NODEOS --data-dir $DATADIR --config-dir $DATADIR "$@" > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
