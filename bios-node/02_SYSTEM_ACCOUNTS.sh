805491668d047c041e1fd5695d655f4dbcc16fcc
Syed Jafri <syed.jafri2@ucalgary.ca>
2018-05-25 05:52:09 -0600

    Readability

--- bios-node/02_SYSTEM_ACCOUNTS.sh
+++ bios-node/02_SYSTEM_ACCOUNTS.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 ################################################################################
 #
 # Script Created by http://CryptoLions.io
 # Edit for Ghostbusters testnet
-#
 #
 ################################################################################
 
 EOSIO_PRODUCER_KEY="$( jq -r '.EOSIO_PRODUCER_PUB_KEY' "00_CONFIG.conf" )"
 cmd="./cleos.sh create account eosio";
 
-echo "Creating system accounts...";
+echo "\n${GREEN}Creating system accounts...";
 
 accounts=( eosio.bpay eosio.msig eosio.names eosio.ram eosio.ramfee eosio.saving eosio.stake eosio.token eosio.vpay );
 
-for a in "${accounts[@]}"
+for account in "${accounts[@]}"
 do
-echo -e "\n creating $a \n";
-$cmd $a $EOSIO_PRODUCER_KEY;
-sleep 1;
+  echo -e "\n creating $account \n";
+  $cmd $account $EOSIO_PRODUCER_KEY;
+  sleep 1;
 done
