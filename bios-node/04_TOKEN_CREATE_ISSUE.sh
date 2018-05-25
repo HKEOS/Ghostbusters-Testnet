39be449344569301a8b7e5d1740ca47beb1297b4
Jae Chung <jae.chung99@gmail.com>
2018-05-25 18:22:22 +0800

    Update 04_TOKEN_CREATE_ISSUE.sh

--- bios-node/04_TOKEN_CREATE_ISSUE.sh
+++ bios-node/04_TOKEN_CREATE_ISSUE.sh
@@ -6,8 +6,8 @@
 #
 ################################################################################
 
-./cleos.sh push action eosio.token create '["eosio", "10000000000.0000 EOS", 0, 0, 0]' -p eosio.token
+./cleos.sh push action eosio.token create '["eosio", "10000000000.0000 EOS"]' -p eosio.token
 ./cleos.sh push action eosio.token issue '["eosio",  "1000000000.0000 EOS", "init"]' -p eosio
 
-#./cleos.sh push action eosio.token create '["eosio", "10000000000.0000 SYS", 0, 0, 0]' -p eosio.token
+#./cleos.sh push action eosio.token create '["eosio", "10000000000.0000 SYS"]' -p eosio.token
 #./cleos.sh push action eosio.token issue '["eosio",  "1000000000.0000 SYS", "init"]' -p eosio
