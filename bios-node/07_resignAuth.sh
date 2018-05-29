03ef25ae790a0a0395f0cfa09d1c21380b0b84a3
Jae Chung <jae.chung99@gmail.com>
2018-05-29 22:27:56 +0800

    Update 07_resignAuth.sh

--- bios-node/07_resignAuth.sh
+++ bios-node/07_resignAuth.sh
@@ -13,8 +13,8 @@ do
   echo -e "\n changing permissions for $abp \n";
   name=(${line//,/ })
   abp="${name[0]}"
-    ./cleos.sh push action eosio updateauth '{"account": "eosio", "permission": "owner", "parent": "", "auth":{"threshold": 1, "keys": [], "waits": [], "accounts": [{"weight": 1, "permission": {"actor": "abp", "permission": active}}]}} -p eosio@active
-    ./cleos.sh push action eosio updateauth '{"account": "eosio", "permission": "active", "parent": "owner", "auth":{"threshold": 1, "keys": [], "waits": [], "accounts": [{"weight": 1, "permission": {"actor": "abp", "permission": active}}]}} -p eosio@active
+    ./cleos.sh push action eosio updateauth '{"account": "eosio", "permission": "owner", "parent": "", "auth":{"threshold": 1, "keys": [], "waits": [], "accounts": [{"weight": 1, "permission": {"actor": "abp", "permission": active}}]}}' -p eosio@active
+    ./cleos.sh push action eosio updateauth '{"account": "eosio", "permission": "active", "parent": "owner", "auth":{"threshold": 1, "keys": [], "waits": [], "accounts": [{"weight": 1, "permission": {"actor": "abp", "permission": active}}]}}' -p eosio@active
     sleep 1;
 done < producers
 
@@ -23,7 +23,7 @@ accounts=( eosio.bpay eosio.msig eosio.names eosio.ram eosio.ramfee eosio.saving
 for account in "${accounts[@]}"
 do
   echo -e "\n changing permissions for $account \n";
-  ./cleos.sh push action eosio updateauth '{"account": "account", "permission": "owner", "parent": "", "auth":{"threshold": 1, "keys": [], "waits": [], "accounts": [{"weight": 1, "permission": {"actor": "eosio", "permission": active}}]}} -p eosio@owner
-  ./cleos.sh push action eosio updateauth '{"account": "account", "permission": "active", "parent": "owner", "auth":{"threshold": 1, "keys": [], "waits": [], "accounts": [{"weight": 1, "permission": {"actor": "eosio", "permission": active}}]}} -p eosio@active
+  ./cleos.sh push action eosio updateauth '{"account": "account", "permission": "owner", "parent": "", "auth":{"threshold": 1, "keys": [], "waits": [], "accounts": [{"weight": 1, "permission": {"actor": "eosio", "permission": active}}]}}' -p eosio@owner
+  ./cleos.sh push action eosio updateauth '{"account": "account", "permission": "active", "parent": "owner", "auth":{"threshold": 1, "keys": [], "waits": [], "accounts": [{"weight": 1, "permission": {"actor": "eosio", "permission": active}}]}}' -p eosio@active
   sleep 1;
 done
